597bb546304a0175334dfe4fc899e47fc52945db
maouai233 <maouai233@outlook.com>
2026-05-02 17:40:03 +0800

    feat: using migrate can convert the old-style config into a new one

--- lua/command_palette.lua
+++ lua/command_palette.lua
@@ -1,5 +1,24 @@
 local M = {}
 
+--- transform a LinArcX-version style config to be applied to the new version
+---@param oldver table -- old-style configurations
+M.migrate = function(oldver)
+  local newver = { "Command Palette" }
+  for _, category in pairs(oldver) do
+    local submenu = { category[1] }
+    for _, command in pairs(category) do
+      if type(command) == "table" then
+        local op = {
+          command[1],
+          command[2],
+        }
+        table.insert(submenu, op)
+      end
+    end
+    table.insert(newver, submenu)
+  end
+  return newver
+end
 M.CpMenu = {}
 
 return M
