3d6183f3fbfa757b9699c0966efcb63d6baa3591
maouai233 <maouai233@outlook.com>
2026-05-02 16:34:10 +0800

    feat!: add submenu supports, drop categories & commands
    BREAKING CHANGE: all the existing config table written as per the
    current README.md will fail due to the addition of sub-menu.

--- lua/telescope/_extensions/command_palette.lua
+++ lua/telescope/_extensions/command_palette.lua
@@ -70,41 +70,92 @@ function themes.vscode(opts)
   return vim.tbl_deep_extend("force", theme_opts, opts)
 end
 
-local function table_length(T)
-  local count = 0
-  for _ in pairs(T) do
-    count = count + 1
-  end
-  return count
+--- create an item to describe a selectable entry
+---@param raw PaletteSelection
+---@return PaletteSelection
+local function make_selection(raw)
+  return {
+    name = selection_attr(raw, "n"),
+    desc = selection_attr(raw, "d"),
+    ops = selection_attr(raw, "o"),
+  }
 end
 
-local function list_of_categories()
-  local results = {}
-  for i = table_length(CpMenu), 1, -1 do
-    results[i] = CpMenu[i][1]
-  end
-  return results
-end
-
-local function list_of_commands(index)
+-- generate a table to display
+-- strip the first item, i.e. name of this menu
+local function extract_menu(menu)
   local results = {}
   local j = 1
-  for i = table_length(CpMenu[index]), 2, -1 do
-    results[j] = CpMenu[index][i]
-    j = j + 1
+  for i = vim.tbl_count(menu), 1, -1 do
+    -- type table means this is a selection
+    if type(menu[i]) == "table" then
+      results[j] = make_selection(menu[i])
+      j = j + 1
+    end
   end
   return results
 end
 
--- picker: commands
-local function commands(opts, table)
+local function menu_name(menu)
+  return menu[1] or menu.name
+end
+
+---@alias PaletteOperation
+---| table sub-level menu
+---| string vimscript
+---| function lua function
+
+---@class PaletteSelection
+---@field name string
+---@field desc string
+---@field ops PaletteOperation
+
+---@alias PaletteAttribute
+---| 'n' name
+---| 'd' description
+---| 'o' operations
+
+--- get an iterm's attribute as per attribute
+---@param selection PaletteSelection
+---@param attribute PaletteAttribute
+---@return string|PaletteOperation|nil
+function selection_attr(selection, attribute)
+  assert(type(selection) == "table")
+  -- get name
+  if attribute == "n" then
+    return selection.name or selection[1]
+  -- get description
+  elseif attribute == "d" then
+    if selection.desc ~= nil then
+      return selection.desc
+    end
+    local desc_slot = selection[3]
+    if desc_slot ~= nil and type(desc_slot) == "string" then
+      return desc_slot
+    end
+  -- get operations
+  elseif attribute == "o" then
+    if selection.ops ~= nil then
+      return selection.ops
+    end
+    local op_slot = selection[2]
+    if op_slot ~= nil and (type(op_slot) == "string" or type(op_slot) == "function") then
+      return op_slot
+    end
+    return extract_menu(selection)
+  end
+  return ""
+end
+
+categories = function(opts, menu)
   opts = opts or {}
   pickers
     .new(opts, {
-      prompt_title = opts.commands_title,
+      prompt_title = menu_name(menu),
       finder = finders.new_table {
-        results = table,
+        results = extract_menu(menu),
         entry_maker = function(entry)
+          local item = entry
           local results_win = vim.api.nvim_get_current_win()
           local w = vim.api.nvim_win_get_width(results_win)
           local h = vim.api.nvim_win_get_height(results_win)
@@ -126,64 +177,40 @@ local function commands(opts, table)
             },
           }
 
+          local name = item.name
+          local desc = item.desc
+          assert(type(name) == "string")
+          assert(type(desc) == "string")
+
           local function make_display()
             return displayer {
-              { entry[1] },
-              { entry[2] },
+              { name },
+              { desc },
             }
           end
 
           return {
-            value = entry,
+            value = item,
             display = make_display,
-            ordinal = string.format("%s %s", entry[1], entry[2]),
+            ordinal = string.format("%s %s", name, desc),
           }
         end,
-      },
-      sorter = conf.generic_sorter(opts),
-      attach_mappings = function(prompt_bufnr, map)
-        map("i", "<C-b>", function()
-          categories(require("telescope.themes").vscode {})
-        end)
-        map("n", "<C-b>", function()
-          categories(require("telescope.themes").vscode {})
-        end)
-        actions.select_default:replace(function()
-          actions.close(prompt_bufnr)
-          -- temporarily workaround for telescope issue: 1599.
-          local selection = action_state.get_selected_entry()
-          if selection.value[3] == 1 then
-            vim.schedule(function()
-              vim.cmd "startinsert! "
-            end)
-          end
-          vim.api.nvim_exec(selection.value[2], true)
-        end)
-        return true
-      end,
-    })
-    :find()
-end
-
-categories = function(opts)
-  opts = opts or {}
-  pickers
-    .new(opts, {
-      prompt_title = "categories",
-      finder = finders.new_table {
-        results = list_of_categories(),
       },
       sorter = conf.generic_sorter(opts),
       attach_mappings = function(prompt_bufnr)
         actions.select_default:replace(function()
           actions.close(prompt_bufnr)
           -- temporarily workaround for telescope issue: 1599.
-          vim.schedule(function()
-            vim.cmd "startinsert! "
-          end)
-          local selection = action_state.get_selected_entry()
-          opts.commands_title = selection[1]
-          commands(opts, list_of_commands(selection.index))
+          -- vim.schedule(function()
+          --   vim.cmd "startinsert! "
+          -- end)
+          ---@type PaletteSelection
+          local op = action_state.get_selected_entry().value.ops
+          if type(op) == "string" then
+            vim.api.nvim_exec2(op, { output = true })
+          elseif type(op) == "table" then
+            categories(opts, op)
+          end
         end)
         return true
       end,
@@ -192,7 +219,7 @@ categories = function(opts)
 end
 
 local function run()
-  categories(require("telescope.themes").vscode {})
+  categories(require("telescope.themes").vscode {}, CpMenu)
 end
 
 return require("telescope").register_extension {
